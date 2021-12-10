750c5b9dcf4b5b9ca20d65fb3e82702293354d29
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-12-10 17:22:29 +0600

    https://cdn.skypack.dev/three

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,46 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
 // import * as THREE from 'three';
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
 class Webgl {
     constructor() {
-        this.camera=this.newCamera();
-        this.scene=this.newScene();
-        this.renderer=this.newRenderer();
-        this.canvas=this.newCanvas();
-
+        this.camera = this.newCamera();
+        this.scene = this.newScene();
+        this.renderer = this.newRenderer();
+        this.canvas = this.newCanvas();
         this.addStartingListener();
+
         this.start();
     }
-    
-    newRenderer () {
+
+    newRenderer() {
         let renderer = new THREE.WebGLRenderer({
             antialias: true
         });
         renderer.setSize(window.innerWidth, window.innerHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
         return renderer;
     }
 
-    
-    newCamera () {
-        return new THREE.PerspectiveCamera(75,
+
+    newCamera() {
+        let camera=new THREE.PerspectiveCamera(75,
             window.innerWidth / window.innerHeight, .1, 1000);
+        camera.position.z=10;
+        return camera;
     }
 
-    
-    newScene () {
+
+    newScene() {
         let scene = new THREE.Scene();
-        scene.background = new THREE.Color(0xffff00);
+        scene.background = new THREE.Color(0x2ecc71);
         return scene;
     }
 
-    
-    newCanvas () {
+
+    newCanvas() {
         let div = document.getElementById("canvas");
         return div.appendChild(this.renderer.domElement);
-    }
-
-    
-    start () {
-        this.renderer.setPixelRatio(window.devicePixelRatio);
-        this.renderer.render(this.scene, this.camera);
     }
 
     addStartingListener() {
@@ -52,12 +50,29 @@ class Webgl {
             this.renderer.setSize(window.innerWidth, window.innerHeight);
         });
     }
+
+    start() {
+        let geometry=new THREE.PlaneGeometry();
+        let material=new THREE.MeshBasicMaterial({side:THREE.DoubleSide});
+        let mesh=new THREE.Mesh(geometry, material);
+        this.scene.add(mesh);
+
+        const controls = new OrbitControls( this.camera, this.renderer.domElement );
+        controls.target.set( 0, 0, 0 );
+        controls.update();
+        controls.addEventListener( 'change', ()=>{this.render();});
+    }
+
+    render(){
+        this.renderer.render(this.scene, this.camera);
+    }
+
 }
 
-let webgl=new Webgl();
+let webgl = new Webgl();
 console.log(webgl);
-const animate=()=> {
-    webgl.renderer.render(webgl.scene, webgl.camera);
+const animate = () => {
+    webgl.render();
     window.requestAnimationFrame(animate);
 }
 animate();
