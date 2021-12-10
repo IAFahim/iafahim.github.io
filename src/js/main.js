c1c89f6e0bff38ef96a0d6ed7b49b8a615639418
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-12-11 03:16:33 +0600

    let see

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
-import * as THREE from 'three';
-import {OrbitControls} from "./three/examples/jsm/controls/OrbitControls";
-// import { FontLoader } from './three/examples/jsm/loaders/FontLoader.js';
-import { TextGeometry } from './three/examples//jsm/geometries/TextGeometry.js';
+import * as THREE from 'https://cdn.skypack.dev/three';
+// import * as THREE from 'three';
+import {OrbitControls} from "https://cdn.skypack.dev/three/example/jsm/controls/OrbitControls.js";
+import { FontLoader } from 'https://cdn.skypack.dev/three/examples/jsm/loaders/FontLoader.js';
+import { TextGeometry } from 'https://cdn.skypack.dev/three/examples//jsm/geometries/TextGeometry.js';
 
 class Webgl {
     static defaultFont;
@@ -53,17 +54,17 @@ class Webgl {
         });
     }
 
-    // fontLoader(){
-    //     const loader = new FontLoader();
-    //     this.font=loader.parse('fonts/gentilis_regular.typeface.json');
-    //     localStorage.setItem('offlineFont',JSON.stringify(this.font));
-    //     let temp=localStorage.getItem('offlineFont');
-    //     if(temp){
-    //         let data=JSON.parse(temp);
-    //         console.log(this.font)
-    //         console.log(data);
-    //     }
-    // }
+    fontLoader(){
+        const loader = new FontLoader();
+        this.font=loader.parse('fonts/gentilis_regular.typeface.json');
+        localStorage.setItem('offlineFont',JSON.stringify(this.font));
+        let temp=localStorage.getItem('offlineFont');
+        if(temp){
+            let data=JSON.parse(temp);
+            console.log(this.font)
+            console.log(data);
+        }
+    }
 
     // addLabel( name, location ) {
     //
