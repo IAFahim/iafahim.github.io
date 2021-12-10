4a4aa8249030b3372c560471c9075867cbb5a174
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2021-12-10 12:27:27 +0600

    https://cdn.skypack.dev/three

--- src/js/main.js
+++ src/js/main.js
@@ -1,36 +1,63 @@
 import * as THREE from 'https://cdn.skypack.dev/three';
 // import * as THREE from 'three';
 
-let renderer, scene, camera, canvas;
+class Webgl {
+    constructor() {
+        this.camera=this.newCamera();
+        this.scene=this.newScene();
+        this.renderer=this.newRenderer();
+        this.canvas=this.newCanvas();
 
-const init = () => {
-    renderer = new THREE.WebGLRenderer({
-        antialias: true
-    });
-    renderer.setClearColor(0xffff00, 1);
-    renderer.setSize(innerWidth, innerHeight);
-    camera = new THREE.PerspectiveCamera(75,
-        innerWidth / innerHeight, .1, 1000);
+        this.addStartingListener();
+        this.start();
+    }
+    
+    newRenderer () {
+        let renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        return renderer;
+    }
 
-    scene=new THREE.Scene();
-    let div= document.getElementById("canvas");
-    canvas=div.appendChild(renderer.domElement);
+    
+    newCamera () {
+        return new THREE.PerspectiveCamera(75,
+            window.innerWidth / window.innerHeight, .1, 1000);
+    }
 
-    addEventListener("resize", ()=>{
-        camera.aspect=innerWidth/innerWidth;
-        camera.updateProjectionMatrix();
-        renderer.setSize(innerWidth, innerHeight);
-    });
+    
+    newScene () {
+        let scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xffff00);
+        return scene;
+    }
 
-    renderer.render(scene, camera);
+    
+    newCanvas () {
+        let div = document.getElementById("canvas");
+        return div.appendChild(this.renderer.domElement);
+    }
+
+    
+    start () {
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    addStartingListener() {
+        window.addEventListener("resize", () => {
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+        });
+    }
 }
 
-const animate=()=>{
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
+let webgl=new Webgl();
+console.log(webgl);
+const animate=()=> {
+    webgl.renderer.render(webgl.scene, webgl.camera);
+    window.requestAnimationFrame(animate);
 }
-
-init();
-
-
-
+animate();
