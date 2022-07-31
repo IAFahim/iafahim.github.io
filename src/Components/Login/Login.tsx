d6fdbbbd13e0ffa5b1c7f117d7b6910e18e6b2a2
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-07-31 13:57:48 +0600

    rebase muha ha ha

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,21 +1,50 @@
-import React, {useState} from "react";
-import Button from "./Button/Button";
-import "./Login.css"
-import {loginWithGoogle, logout} from '../../SupaBase/SupabseUni'
+import { useState } from 'react'
+import { supabase } from '../../SupaBase/SupabseUni'
 
-function Login() {
-    let [data, setData]=useState();
+export default function Auth() {
+    const [loading, setLoading] = useState(false)
+    const [email, setEmail] = useState('')
 
-    const loginHandler = () => {
-        let data = {...loginWithGoogle("google")}
+    const handleLogin = async (e:any) => {
+        e.preventDefault()
+
+        try {
+            setLoading(true)
+            const { error } = await supabase.auth.signIn({ email })
+            if (error) throw error
+            alert('Check your email for the login link!')
+        } catch (error) {
+            // @ts-ignore
+            alert(error.error_description || error.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
     return (
-        <div className="login">
-            <Button text={"Login with Google"} onclick={loginHandler()}/>
-            <p>{data}</p>
+        <div className="row flex flex-center">
+            <div className="col-6 form-widget" aria-live="polite">
+                <h1 className="header">Supabase + React</h1>
+                <p className="description">Sign in via magic link with your email below</p>
+                {loading ? (
+                    'Sending magic link...'
+                ) : (
+                    <form onSubmit={handleLogin}>
+                        <label htmlFor="email">Email</label>
+                        <input
+                            id="email"
+                            className="inputField"
+                            type="email"
+                            placeholder="Your email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <button className="button block" aria-live="polite">
+                            Send magic link
+                        </button>
+                    </form>
+                )}
+            </div>
         </div>
-    );
+    )
 }
-
-export default Login;
