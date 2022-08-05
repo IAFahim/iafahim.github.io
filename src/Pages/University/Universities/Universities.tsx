8be2d431f1141cbef3dbda09f295b24850a2356a
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-08-05 15:31:55 +0600

    Universities

--- src/Pages/University/Universities/Universities.tsx
+++ src/Pages/University/Universities/Universities.tsx
@@ -1,24 +1,29 @@
 import {MultiSelect, Card, Container, useMantineTheme} from '@mantine/core';
 import COUNTRY_NAMES from "../Data/countryNames";
 import {useState} from "react";
-export default function Universities() {
-    const [value, setValue] = useState([] );
 
-    const handleChange = (e:any) => {
+export default function Universities() {
+    const [value, setValue] = useState([]);
+
+    const handleChange = (e: any) => {
         getUniversityList(e[0])
         setValue(e)
     }
 
-    const getUniversityList = async (country:string) => {
-
+    const getUniversityList = async (country: string) => {
+        fetch("https://raw.githubusercontent.com/IAFahim/iafahim.github.io/master/src/Pages/University/" + country.toLowerCase() + ".json").then(e => {
+            e.json().then(data=>
+                console.log(data)
+            )
+        })
     }
 
     return (
         <Container>
-            <MultiSelect style={{maxWidth: 400, overflow:'hidden'}}
+            <MultiSelect style={{maxWidth: 400, overflow: 'hidden'}}
                          data={COUNTRY_NAMES}
                          value={value}
-                        // @ts-ignore
+                // @ts-ignore
                          onChange={handleChange}
                          label="Your favorite frameworks/libraries"
                          placeholder="Pick all that you like"
