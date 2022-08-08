d80e779b666b5e9fd5a586b760c6dff94ef398c3
fahimmanowarj5 <fahimmanowarj5@gmail.com>
2022-08-09 01:29:56 +0600

    Sqllllllll

--- open-sql-schema/create_tables.sql
+++ open-sql-schema/create_tables.sql
@@ -1,10 +1,10 @@
 CREATE EXTENSION hstore;
 
-create table users
+create table profiles
 (
     id              bigserial primary key,
-    userName        varchar(30) unique not null check ( length(userName) > 2 and users.userName ~* '[a-z]'),
-    user_id         uuid,
+    name            varchar(50) unique           not null check ( length(name) > 2 and profiles.name ~* '[a-z]'),
+    uuid            uuid references auth (users) not null,
     logo_url        varchar,
     email           varchar(128),
     university_id   int,
@@ -13,18 +13,75 @@ create table users
     visit_history   hstore
 );
 
-insert into users(userName)values ('wht 2 wiie');
-insert into users(userName)values ('fahim2');
+create or replace function public.handle_new_user()
+    returns trigger
+    language plpgsql
+    security definer set search_path = public
+as
+$$
+begin
+    insert into profiles (uuid, name)
+    values (new.users, new.users);
+    return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create or replace trigger on_auth_user_created
+    after insert
+    on auth
+    for each row
+execute procedure handle_new_user();
+
+create table profile_joining_club
+(
+    profiles_id bigint primary key references profiles (id),
+    clubs_id    bigint references profiles (id),
+    class       int default 0
+);
+
+create table club
+(
+    id                   bigserial primary key,
+    joined_student_count int default 1,
+    created_by           bigint references profiles (id),
+    name                 varchar(100),
+    description          varchar,
+    visit                hstore,
+    social_websites      hstore
+);
+
+insert into club(created_by, name, description, social_websites)
+values (1, 'NSU-ACM', 'Lots of fun', '"FaceBook"=>"facebook.com"');
+
+insert into club(created_by, name, description, social_websites)
+values (3, 'NSU-YES', 'Lots of fun But YES', '"LinkedIN"=>"linkedin.com"');
+
+
+select *
+from profiles
+         left join club c on profiles.id = c.created_by;
+
+insert into profiles(name, uuid)
+values ('IAFahim', '78da1732-300b-4dc0-9380-d05516f8d550'::uuid);
+insert into profiles(name, uuid)
+values ('Spondon', '0478acd4-8c5d-42e2-8b42-94e4b75f7b79'::uuid);
+
+insert into auth(users)
+values ('78da1732-300b-4dc0-9380-d05516f8d550'::uuid);
+
+insert into auth(users)
+values ('0478acd4-8c5d-42e2-8b42-94e4b75f7b79'::uuid);
 
 create table auth
 (
-    id                 uuid not null primary key,
+    users              uuid not null primary key,
     email              varchar,
     raw_user_meta_data jsonb,
     phone              varchar
 );
 
-insert into auth (id, email, raw_user_meta_data, phone)
+insert into auth (users, email, raw_user_meta_data, phone)
 values ('78da1732-300b-4dc0-9380-d05516f8d550', 'fahimmanowarj5@gmail.com', '{
   "iss": "https://www.googleapis.com/userinfo/v2/me",
   "sub": "109365051886332781261",
